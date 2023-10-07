7153e689178825f6c2953accc37708baf15854eb
Aa20475 <aa20475@gmail.com>
2023-10-07 19:34:52 -0400

    Chat + Summarise

--- content.js
+++ content.js
@@ -1,3 +1,42 @@
+var chatHistory = [
+]
+
+
+function addChatBubble(){
+    var chat = chatHistory[chatHistory.length -1];
+    const chatBubbleContainer = document.createElement("div");
+    const chatBubble = document.createElement('div');
+    const chatWindow = document.getElementById('chat-window');
+    chatBubbleContainer.classList.add("pb-3")
+    chatBubble.classList.add('chat-bubble');
+
+    if (chat.isUser) {
+        chatBubbleContainer.classList.add('d-flex');
+        chatBubbleContainer.classList.add('justify-content-end');
+        chatBubble.classList.add("bg-primary")
+    } else {
+        chatBubbleContainer.classList.add('d-flex');
+        chatBubbleContainer.classList.add('justify-content-start');
+    }
+    chatBubbleContainer.classList.add('align-items-center');
+
+
+    chatBubble.textContent = chat.text;
+    chatBubbleContainer.appendChild(chatBubble);
+    chatWindow.appendChild(chatBubbleContainer);
+}
+
+function getChatResponse(chat){
+    chatHistory.push(
+        {
+            "id" : chatHistory.length,
+            "text" : "chatgpt response",
+            "isUser" : false
+        }
+    )
+}
+
+
 function fetchSummary() {
   const currentPageUrl = encodeURIComponent(window.location.href);
   // const currentPageUrl = window.location.href;
@@ -16,10 +55,43 @@ function injectPane(htmlContent) {
   const parser = new DOMParser();
   const doc = parser.parseFromString(htmlContent, 'text/html');
   const pane = doc.querySelector('#container');
+
   document.body.appendChild(pane);
 
+  const chatContainer = document.getElementById('chat-container')
+  const chatbox = document.getElementById('chat-box')
+  const chat = document.getElementById('chat')
+
+  chatContainer.setAttribute("style", "height:" + Math.floor(0.8 * chat.clientHeight) + "px !important");
+  chatbox.setAttribute("style", "height:" + (chat.clientHeight - Math.floor(0.8 * chat.clientHeight)) + "px !important");
+  
+  const textarea = document.querySelector("#chat-textarea")
+  textarea.addEventListener('input', function () {
+    charCount.textContent = textarea.value.length;
+  });
+
+  textarea.addEventListener('keypress', function (event) {
+    // Get the key code of the key pressed
+    if(event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        console.log("Submit chat!");
+        chatHistory.push(
+            {
+                "id" : chatHistory.length,
+                "text" : textarea.value,
+                "isUser" : true
+            }
+        )
+        addChatBubble();
+        textarea.value = "";
+        getChatResponse();
+        addChatBubble(); 
+    }
+  });
+
+
   // Event listeners
-  document.getElementById('closeIcon').addEventListener('click', function() {
+  document.querySelector('#closeIcon').addEventListener('click', function() {
       pane.style.display = 'none';
   });
 
